765a0a004da6270efa4b98f8c588c8e466d84280
Allen Chen <alchen@drwholdings.com>
2023-04-13 16:30:24 -0500

    update mason

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = {"pyright"},
+      ensure_installed = {},
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,7 +13,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "prettier", "stylua", "black", "jq", "pyproject-flake8"},
+      ensure_installed = { "prettier", "stylua", "black", "jq", "ruff"},
     },
   },
   {
