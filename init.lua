a9948e812bb47c33469810fc6b244458d7ba0b85
Allen Chen <alchen@drwholdings.com>
2023-04-28 16:45:35 -0500

    tokyomoon

--- init.lua
+++ init.lua
@@ -18,8 +18,8 @@ return {
   },
 
   -- Set colorscheme to use
-  -- colorscheme = "tokyonight-night",
-  colorscheme = "catppuccin-mocha",
+  colorscheme = "tokyonight-moon",
+  -- colorscheme = "catppuccin-mocha",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
