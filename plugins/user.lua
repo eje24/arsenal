5962a8d5501f7f833871e25c1080eb50aa25052f
Allen Chen <alchen@drwholdings.com>
2023-06-17 17:10:29 -0500

    better home

--- plugins/user.lua
+++ plugins/user.lua
@@ -100,4 +100,14 @@ return {
     lazy = false,
     config = function() require("catppuccin").setup {} end,
   },
+  {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      require("dashboard").setup {
+        -- config
+      }
+    end,
+    dependencies = { { "nvim-tree/nvim-web-devicons" } },
+  },
 }
